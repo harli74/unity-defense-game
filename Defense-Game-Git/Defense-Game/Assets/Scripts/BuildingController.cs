23e07883e28ae603346dd021c3fbbf1f6b5103f6
golfeto-PC\golfeto <milen316@gmail.com>
2020-07-03 23:26:31 +0300

    Building_sys_almost_rdy

---
+++ Defense-Game-Git/Defense-Game/Assets/Scripts/BuildingController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour
+{
+    public RaycastHit hit;
+    public Camera cam;
+
+    public float greenX, greenZ;
+
+    public GameObject map;
+    public GameObject greenBox;
+    public GameObject redBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        greenX = Mathf.Round(hit.point.x);
+        greenZ = Mathf.Round(hit.point.z);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (hit.point.x <= map.transform.localScale.x && hit.point.z <= map.transform.localScale.z && hit.point.x >= -map.transform.localScale.x && hit.point.z >= -map.transform.localScale.z)
+            {
+                Debug.Log("im In");
+            }
+
+        }
+    }
+    void GreenOrRedBoxChecked()
+    {
+
+    }
+}
