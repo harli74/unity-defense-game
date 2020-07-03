2fc13ae6e71b018b9353df50e72d003b6de655b7
golfeto-PC\golfeto <milen316@gmail.com>
2020-07-03 23:35:41 +0300

    building_sys_Still_on_Progress

--- Defense-Game-Git/Defense-Game/Assets/Scripts/BuildingController.cs
+++ Defense-Game-Git/Defense-Game/Assets/Scripts/BuildingController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BuildingController : MonoBehaviour
 {
@@ -10,8 +11,16 @@ public class BuildingController : MonoBehaviour
     public float greenX, greenZ;
 
     public GameObject map;
+    public GameObject SpawnableObj;
     public GameObject greenBox;
+    public GameObject greenClone;
     public GameObject redBox;
+    public GameObject redClone;
+
+    public bool RedMapCheck;
+    public bool SpawnedGreenAlready;
+    public bool SpawnedRedAlready;
+    public bool SpawnableObjOn;
 
     // Start is called before the first frame update
     void Start()
@@ -26,18 +35,77 @@ public class BuildingController : MonoBehaviour
         greenZ = Mathf.Round(hit.point.z);
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
+      
+      
+
+
+
         if (Physics.Raycast(ray, out hit))
         {
 
             if (hit.point.x <= map.transform.localScale.x && hit.point.z <= map.transform.localScale.z && hit.point.x >= -map.transform.localScale.x && hit.point.z >= -map.transform.localScale.z)
             {
-                Debug.Log("im In");
-            }
+                RedMapCheck = false;
 
+                if (RedMapCheck == false)
+                {
+                    SpawnedRedAlready = false;
+                    Debug.Log("im In");
+                    Destroy(redClone);
+
+                    
+                    if (SpawnedGreenAlready == false)
+                    {
+                        GreenBoxChecked();
+                    }
+                  greenClone.transform.position = new Vector3(greenX, 1, greenZ);
+                    if(SpawnableObjOn == false)
+                    {
+                        SpawnObject();
+                        SpawnableObjOn = false;
+                        
+                    }
+                }
+
+            }
+            }
+            else
+            {
+                RedMapCheck = true;
+            SpawnedGreenAlready = false;
+                Debug.Log("im out");
+            
+            Destroy(greenClone);
+            if (SpawnedRedAlready == false)
+            {
+                RedBoxChecked();
+            }
+            else
+            {
+                ///redClone.transform.position = new Vector3(Input.mousePosition.x, 0, Input.mousePosition.z);
+              
+                Debug.Log("im out");
+            }
+        }
+        }
+        void GreenBoxChecked()
+        {
+            greenClone = Instantiate(greenBox, new Vector3(greenX, 1, greenZ), Quaternion.identity);
+        SpawnedGreenAlready = true;
+            
+        }
+    void SpawnObject()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+           GameObject gmobj = Instantiate(SpawnableObj, new Vector3(greenX, 1, greenZ), Quaternion.identity);
+            SpawnableObjOn = true;
         }
     }
-    void GreenOrRedBoxChecked()
-    {
-
+        void RedBoxChecked()
+        {
+            redClone = Instantiate(redBox, new Vector3(greenX, 1, greenZ), Quaternion.identity);
+        SpawnedRedAlready = true;
     }
+    
 }
